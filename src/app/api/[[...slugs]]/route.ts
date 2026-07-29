c7086ffc42e2385b45357517a2696d13c5865564
Manuel Bajos <mbajosr@hotmail.com>
2026-07-29 00:42:09 -0600

    feat: se añade ruta para subir mensajes y la librería de upstash para tiempo real

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,8 @@
 import { Elysia, t } from 'elysia'
 import { nanoid } from 'nanoid'
 import {redis} from "../../../lib/redis"
+import { authMiddleware } from './auth'
+import {z} from "zod"
 
 const ROOM_TTL_SECONDS = 60 * 10
 
@@ -15,6 +17,25 @@ post("/create", async ()=>{
     await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
 
     return {roomId}
+})
+
+const messages = new Elysia({prefix:"/messages"})
+.use(authMiddleware)
+.post("/",async ({body, auth}) =>{
+    const {sender, text} = body
+    const {roomId} = auth
+
+    const roomExists = await redis.exists(`meta:${roomId}`)
+
+    if(!roomExists){
+        throw new Error("Room doesn't exist")
+    }
+}, {
+    query: z.object({roomId: z.string() }),
+    body: z.object({
+        sender: z.string().max(100),
+        text:z.string().max(1000),
+    })
 })
 
 const App = new Elysia({ prefix: '/api' }).use(rooms)
