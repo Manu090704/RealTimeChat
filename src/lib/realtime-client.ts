fc9ccb2d823f8ccc9c588dc9f9fd306a76b174df
Manuel Bajos <mbajosr@hotmail.com>
2026-07-30 13:45:06 -0600

    feat: se añade los archivos para el chat en tiempo real con upstash

---
+++ src/lib/realtime-client.ts
@@ -0,0 +1,5 @@
+"use client"
+import { createRealtime } from "@upstash/realtime/client"
+import type { RealTimeEvents } from "./realtime"
+
+export const {useRealtime} = createRealtime<RealTimeEvents>()
